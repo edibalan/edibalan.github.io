8c9ef219bd02297e8bbad311e106c5f4ec902856
Edi Bălan <142690701+edibalan@users.noreply.github.com>
2023-09-25 23:21:36 +0300

    Update AboutAnimation.js

--- handlers/AboutAnimation.js
+++ handlers/AboutAnimation.js
@@ -4,22 +4,22 @@ export default class AboutAnimation {
   constructor() {
     this.background = QS("#about-background");
 
-    this.aboutAnimationEnd = () => {
-      this.background.src = "./assets/about-background-reverse.gif";
-      this.background.removeEventListener("mouseover", this.aboutAnimationEnd);
-  
-      setTimeout(() => this.background.classList.remove("about-animation"), 5750);
-      setTimeout(() => this.background.addEventListener("mouseover",this.aboutAnimationStart), 7000);
-    };
-  
-    this.aboutAnimationStart = () => {
+    this.launchAnimation = () => {
       this.background.classList.add("about-animation");
       this.background.src = "./assets/about-background.gif";
-      this.background.removeEventListener("mouseover", this.aboutAnimationStart);
-  
-      setTimeout(() => this.background.addEventListener("mouseover", this.aboutAnimationEnd), 7000);
+      this.background.removeEventListener("mouseover", this.launchAnimation);
+      
+      setTimeout(() => this.background.addEventListener("mouseover", this.turnOffAnimation), 7000);
+    };
+
+    this.turnOffAnimation = () => {
+      this.background.src = "./assets/about-background-reverse.gif";
+      this.background.removeEventListener("mouseover", this.turnOffAnimation);
+      
+      setTimeout(() => this.background.classList.remove("about-animation"), 5750);
+      setTimeout(() => this.background.addEventListener("mouseover", this.launchAnimation), 7000);
     };
   }
 
-  initiate() { this.background.addEventListener("mouseover", this.aboutAnimationStart) }
+  initiate() { this.background.addEventListener("mouseover", this.launchAnimation) }
 }
