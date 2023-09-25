54622e9b68e12e97b9462b66e34b99a915316e1d
Edi Bălan <142690701+edibalan@users.noreply.github.com>
2023-09-25 23:22:23 +0300

    Update NavigationHandler.js

--- handlers/NavigationHandler.js
+++ handlers/NavigationHandler.js
@@ -3,75 +3,92 @@ export default class NavigationHandler {
   constructor() {
     this.clickSound = QS("#click-sound");
     this.footer = QS("footer");
-    this.home = {
-      button: QS(".home-button"),
+    
+    this.header = {
       closeNavBtn: QS("#close-navigation-button"),
-      header: QS(".header"),
       navigation: QS(".navigation"),
       navAside: QS(".navigation-aside"),
+      navLinks: QSAll(".navigation-link"),
       openNavBtn: QS("#open-navigation-button"),
-      resumeBtn: QS(".resume-button")
+      resumeBtn: QS(".resume-button"),
+      section: QS(".header")
     };
 
     this.main = QS("main");
-    this.navLinks = QSAll(".navigation-link");
+    this.homeButton = QS(".home-button");
     this.previousPosition = window.scrollY;
 
     this.closeNavMenu = () => {
+      this.header.navigation.classList.remove("show-nav-menu");
       this.main.classList.remove("blurred");
-      this.home.navigation.classList.remove("show-nav-menu");
+
       this.footer.classList.remove("blurred");
       this.footer.removeAttribute("style");
 
-      setTimeout(() => this.home.header.classList.remove("hide-home-header"), 250);
+      setTimeout(() => this.header.section.style.top = 0, 250);
     };
 
     this.openNavMenu = () => {
-      this.home.header.classList.add("hide-home-header");
-      this.home.button.classList.add("hidden-element");
+      this.header.section.style.top = "-9rem";
+      this.homeButton.classList.add("hidden-element");
 
-      setTimeout(() => this.home.navigation.classList.add("show-nav-menu"), 250);
+      setTimeout(() => this.header.navigation.classList.add("show-nav-menu"), 250);
       setTimeout(() => {
         this.main.classList.add("blurred");
-        
-        if (this.footer.classList.contains("light-mode-filter")) {
-          this.footer.style.filter = "invert() blur(0.5rem)"
-        } else {
-          this.footer.classList.add("blurred");
-        };
+        this.footer.classList.contains("light-mode-filter")
+          ? this.footer.style.filter = "invert() blur(0.5rem)"
+          : this.footer.classList.add("blurred");
       }, 350);
+    };
+
+    this.headerHandler = () => {
+      let currentPosition = window.scrollY;
+
+      if (this.header.navigation.classList.contains("show-nav-menu")) this.closeNavMenu();
+
+      this.previousPosition > currentPosition
+        ? setTimeout(() => this.header.section.style.top = 0, 400)
+        : this.header.section.style.top = "-9rem";
+        this.previousPosition = currentPosition;
+    
+      window.scrollY > window.innerHeight
+        ? this.homeButton.classList.remove("hidden-element")
+        : this.homeButton.classList.add("hidden-element");
+    };
+
+    this.navigationHandler = () => {
+      if (window.innerWidth <= 833) {
+        this.header.closeNavBtn.addEventListener("click", this.closeNavMenu);
+        this.header.openNavBtn.addEventListener("click", this.openNavMenu);
+
+        this.header.navLinks.forEach(link => {
+          link.classList.remove("fs-x-small");
+          link.classList.add("fs-medium");
+        });
+
+        this.header.resumeBtn.classList.remove("fs-x-small");
+        this.header.resumeBtn.classList.add("fs-small");
+      }
+      else {
+        this.header.closeNavBtn.removeEventListener("click", this.closeNavMenu);
+        this.header.openNavBtn.removeEventListener("click", this.openNavMenu);
+
+        this.header.navLinks.forEach(link => {
+          link.classList.remove("fs-medium");
+          link.classList.add("fs-x-small");
+        });
+
+        this.header.resumeBtn.classList.remove("fs-small");
+        this.header.resumeBtn.classList.add("fs-x-small");
+      };
     };
   }
 
   initiate() {
-    if (window.innerWidth <= 1024 && screen.orientation.type === "portrait-primary") {
-      window.addEventListener("scroll", () => {
-        let currentPosition = window.scrollY;
-
-        if (this.home.navigation.classList.contains("show-nav-menu")) this.closeNavMenu();
-    
-        this.previousPosition > currentPosition
-          ? setTimeout(() => this.home.header.classList.remove("hide-home-header"), 400)
-          : this.home.header.classList.add("hide-home-header");
-          this.previousPosition = currentPosition;
-      
-        window.scrollY > window.innerHeight
-          ? this.home.button.classList.remove("hidden-element")
-          : this.home.button.classList.add("hidden-element");
-      });
-    };
-
-    if (window.innerWidth <= 834 && screen.orientation.type === "portrait-primary") {
-      this.home.closeNavBtn.addEventListener("click", this.closeNavMenu);
-      this.home.openNavBtn.addEventListener("click", this.openNavMenu);
-
-      this.home.resumeBtn.classList.remove("fs-x-small");
-      this.home.resumeBtn.classList.add("fs-small");
-
-      this.navLinks.forEach(link => {
-        link.classList.remove("fs-x-small");
-        link.classList.add("fs-medium");
-      });
-    };
+    window.addEventListener("resize", this.headerHandler);
+    window.addEventListener("resize", this.navigationHandler);
+    window.addEventListener("resize", this.closeNavMenu);
+    window.addEventListener("scroll", this.headerHandler);
+    this.navigationHandler();
   }
 }
