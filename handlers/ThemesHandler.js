fdcbe3505f44582bd9daf76e0d46a9df0fc2748a
Edi Bălan <142690701+edibalan@users.noreply.github.com>
2023-09-25 23:25:52 +0300

    Update ThemesHandler.js

--- handlers/ThemesHandler.js
+++ handlers/ThemesHandler.js
@@ -8,10 +8,11 @@ export default class ThemesHandler {
       lightMode: QS("#light-on")
     };
     
+    this.contactLinks = QSAll(".contact-nav-menu-link");
     this.contact = {
-      content: QS(".contact-content"),
+      button: QSAll(".page-button")[5],
       copyright: QS(".copyright-container"),
-      links: QSAll(".contact-nav-menu-link"),
+      title: QS(".contact-title"),
       underline: QS("#contact-title-underline"),
       section: QS("#contact")
     };
@@ -24,34 +25,33 @@ export default class ThemesHandler {
       popup: QS(".popup")
     };
 
-    this.projectsBannersArr = QSAll(".projects-banner");
+    this.projectsBanners = QSAll(".projects-banner");
     
     this.darkMode = () => {
       this.home.logo.removeEventListener("click", this.darkMode);
       this.home.logo.addEventListener("click", this.lightMode);
 
       this.audio.darkMode.play();
-      this.contact.underline.classList.remove("light-mode-border");
       this.favicon.href = "/assets/favicon.png";
       this.home.avatar.classList.remove("light-mode-outline");
       this.home.popup.classList.add("hidden-element");
   
-      QSAll(".avatar-container, #about-background, #contact-banner, html").forEach(element => {
-        element.classList.remove("light-mode-filter")
+      QSAll("html, .avatar-container, #about-background").forEach(element => {
+        element.classList.remove("light-mode-filter");
       });
+  
+      for (let i = 0; i < this.projectsBanners.length - 1; i++) {
+        this.projectsBanners[i].classList.remove("light-mode-filter")};
 
-      this.contact.links.forEach(link => {
+      for (let values in this.contact) {
+        this.contact[values].classList.remove("light-mode-filter")};
+     
+      this.contact.section.classList.remove("light-mode-border");
+      this.contactLinks.forEach(link => {
+        link.classList.remove("light-mode-filter");
         link.classList.remove("dark-mode-border");
         link.classList.remove("dark-mode-color");
       });
-
-      this.projectsBannersArr.forEach(banner => banner.classList.remove("light-mode-filter"));
-
-      if (window.innerWidth <= 1024 && screen.orientation.type === "portrait-primary") {
-        this.contact.section.classList.remove("light-mode-filter");
-        this.contact.content.classList.remove("light-mode-filter");
-        this.contact.copyright.classList.remove("light-mode-filter");
-      };
     };
 
     this.lightMode = () => {
@@ -59,29 +59,26 @@ export default class ThemesHandler {
       this.home.logo.addEventListener("click", this.darkMode);
 
       this.audio.lightMode.play();
-      this.contact.underline.classList.add("light-mode-border");
       this.favicon.href = "/assets/favicon-inverted.png";
       this.home.avatar.classList.add("light-mode-outline");
       this.home.popup.classList.add("hidden-element");
-  
-      QSAll(".avatar-container, #about-background, #contact-banner, html").forEach(element => {
+
+      QSAll("html, .avatar-container, #about-background").forEach(element => {
         element.classList.add("light-mode-filter");
       });
+  
+      for (let i = 0; i < this.projectsBanners.length - 1; i++) {
+        this.projectsBanners[i].classList.add("light-mode-filter")};
 
-      this.contact.links.forEach(link => {
+      for (let values in this.contact) {
+        this.contact[values].classList.add("light-mode-filter")};
+     
+      this.contact.section.classList.add("light-mode-border");
+      this.contactLinks.forEach(link => {
+        link.classList.add("light-mode-filter");
         link.classList.add("dark-mode-border");
         link.classList.add("dark-mode-color");
       });
-
-      for (let i = 0; i < this.projectsBannersArr.length - 1; i++) {
-        this.projectsBannersArr[i].classList.add("light-mode-filter");
-      };
-
-      if (window.innerWidth <= 1024 && screen.orientation.type === "portrait-primary") {
-        this.contact.section.classList.add("light-mode-filter");
-        this.contact.content.classList.add("light-mode-filter");
-        this.contact.copyright.classList.add("light-mode-filter");
-      };
     };
   }
 
