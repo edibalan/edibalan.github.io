b7bf09bf2434ecf23f7200eadbf6e49f22f995e3
Edi Bălan <142690701+edibalan@users.noreply.github.com>
2023-09-30 14:41:34 +0300

    Update About.js

--- components/About.js
+++ components/About.js
@@ -13,9 +13,7 @@ export default class About {
   render() {
     return `
       <section class="about-section | container" id="about">
-        <a class="page-button previous-button | fs-sm-large" id="home-link" href="#home">
-          <i class="fa-solid fa-circle-chevron-left"></i>
-        </a>
+        <a class="page-button previous-button | fs-sm-large" id="home-link" href="#home"><i class="fa-solid fa-circle-chevron-left"></i></a>
         
         <div class="about-container">
           <h1 class="about-title | fs-large fw-bold invisible">Greetings !</h1>
@@ -23,15 +21,11 @@ export default class About {
             <div class="about-description | fs-small fw-light">
               ${this.renderAboutDescription()}
             </div>
-            <img alt="about-section-background" class="about-background" id="about-background"
-              src="./assets/about-background.jpeg" />
+            <img alt="about-section-background" class="about-background" id="about-background" src="./assets/about-background.webp" />
           </div>
         </div>
 
-        <a class="page-button next-button | fs-sm-large" href="#projects">
-          <i class="fa-solid fa-circle-chevron-right"></i>
-        </a>
-
+        <a class="page-button next-button | fs-sm-large" href="#projects"><i class="fa-solid fa-circle-chevron-right"></i></a>
         <a aria-label="home-button" class="home-button | hidden-element fs-medium fw-sm-bold" href="#home">❮</a>
       </section>
     `;
