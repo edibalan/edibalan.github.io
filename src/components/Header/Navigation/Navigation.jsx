d9507ed2b70e3df50337b27d0748be12807d5ecd
edibalan <edibalan97@gmail.com>
2024-06-04 15:37:18 +0300

    new resume link

--- src/components/Header/Navigation/Navigation.jsx
+++ src/components/Header/Navigation/Navigation.jsx
@@ -57,7 +57,7 @@ const Navigation = props => {
         <a
           aria-label='Resume Button'
           className='resume__button'
-          href='https://drive.google.com/file/d/1s1o8o8NkClPGF-64dGyhofydXUeUQlkx/view?usp=sharing'
+          href='https://drive.google.com/file/d/1Whjnei63-Ax8a7D-KYObR850vLz9L79f/view?usp=sharing'
           target='_blank'
           title={
             window.innerWidth > window.innerHeight
